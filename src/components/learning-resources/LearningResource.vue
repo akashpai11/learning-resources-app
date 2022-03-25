539ad3cdadeafee0ecf62e0ea03895a7b30c99ab
Akash Pai <68583296+akashpai11@users.noreply.github.com>
2022-03-25 18:02:25 +0530

    Component which serves as a list item

---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <li>
+      <div>
+        <header>
+          <h3>{{ title }}</h3>
+          <button>Delete</button>
+        </header>
+      </div>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+p {
+  margin: 0.5rem 0;
+}
+a {
+  text-decoration: none;
+  font-weight: bold;
+  color: #ce5c00;
+}
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
