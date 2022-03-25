445a12bd1f70b0c96f89e7a48e05f27a2b4e2001
Akash Pai <68583296+akashpai11@users.noreply.github.com>
2022-03-25 18:05:31 +0530

    Added the stored resources component with styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <ul>
-    <li>Hi</li>
-  </ul>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
 export default {
   data() {
     return {
@@ -15,10 +14,31 @@ export default {
           description: 'The Official VUE JS docs!',
           link: 'https://vuejs.org',
         },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'The most important skill to learn is - How to Google ?',
+          link: 'https://google.com',
+        },
       ],
     };
   },
+  components: { StoredResources },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,700;1,600&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Nunito Sans', Segoe UI, Tahoma, Geneva, Verdana, sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="item in resources"
+      :key="item.id"
+      :title="item.title"
+      :description="item.description"
+      :link="item.link"
+    />
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+export default {
+  props: ['resources'],
+  components: {
+    LearningResource,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
