ea25a1ce26dc1a7526d45c91179ab4d3088cfdcc
Akash Pai <68583296+akashpai11@users.noreply.github.com>
2022-03-25 15:12:57 +0530

    Basic changes

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li>Hi</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official VUE JS docs!',
+          link: 'https://vuejs.org',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
